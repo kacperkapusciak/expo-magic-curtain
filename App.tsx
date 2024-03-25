d1fbac0f0ab99922269e25afd7f23754f31927dc
kacperkapusciak <kacper.kapusciak@swmansion.com>
2024-03-25 11:02:29 +0100

    Extract shared logic

--- App.tsx
+++ App.tsx
@@ -23,7 +23,6 @@ import {
   ImageShader,
   makeImageFromView,
   Shader,
-  Skia,
   type SkImage,
 } from "@shopify/react-native-skia";
 import Animated, {
@@ -34,54 +33,11 @@ import Animated, {
   useSharedValue,
   withTiming,
 } from "react-native-reanimated";
+import { Transition, glsl, transition } from "./utils/shader";
 
 const TRANSITION_DURATION = 800;
 
 const { width, height } = Dimensions.get("window");
-
-type Value = string | number;
-type Values = Value[];
-
-export const glsl = (source: TemplateStringsArray, ...values: Values) => {
-  const processed = source.flatMap((s, i) => [s, values[i]]).filter(Boolean);
-  return processed.join("");
-};
-
-export const frag = (source: TemplateStringsArray, ...values: Values) => {
-  const code = glsl(source, ...values);
-  const rt = Skia.RuntimeEffect.Make(code);
-  if (rt === null) {
-    throw new Error("Couln't Compile Shader");
-  }
-  return rt;
-};
-
-type Transition = string;
-
-const transition = (t: Transition) => {
-  return frag`
-  uniform shader image1;
-  uniform shader image2;
-
-  uniform float progress;
-  uniform float2 resolution;
-  
-  half4 getFromColor(float2 uv) {
-    return image1.eval(uv * resolution);
-  }
-  
-  half4 getToColor(float2 uv) {
-    return image2.eval(uv * resolution);
-  }
-  
-  ${t}
-
-  half4 main(vec2 xy) {
-    vec2 uv = xy / resolution;
-    return transition(uv);
-  }
-  `;
-};
 
 const wipeLeft: Transition = glsl`
 // Author: Jake Nelson
